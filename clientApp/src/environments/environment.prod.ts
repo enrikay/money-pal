9b063c1e46c7a5433092e86eadaedfa0d257b7b9
enrikay <126099598+enrikay@users.noreply.github.com>
2023-03-30 20:57:28 +0100

    update prod

--- clientApp/src/environments/environment.prod.ts
+++ clientApp/src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
-  API_URL: 'https://ozrokuwspk.execute-api.us-east-1.amazonaws.com/api/v1/'
+  API_URL: {
+    user: 'http://localhost:1234/api/user/',
+    wallet: 'http://localhost:2345/api/wallet/',
+    payment: 'http://localhost:3456/api/payment/',
+    billing: 'http://localhost:4567/api/billing/'
+	}
 };
